0afc818364848fee3d370107bc3f0e657b6c749a
Naman80 <naman.goel80@gmail.com>
2023-07-13 21:04:16 +0530

    added files

---
+++ src/components/Album.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Album() {
+  return (
+    <div className="album_container">
+      <div className="album_thumbnail">this is my thumbnail image</div>
+      <h1 className="album_title">album title</h1>
+    </div>
+  );
+}
+
+export default Album;
