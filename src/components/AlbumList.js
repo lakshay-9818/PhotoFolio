dab2e4d3cd35fa0ecf673c1b823836ba16ba553d
Lakshay Gupta <lakshaygupta3110@gmail.com>
2023-07-13 23:49:26 +0530

    album tiles

---
+++ src/components/AlbumList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import AlbumTile from './AlbumTile'
+
+function AlbumList() {
+  return (
+    <div className='border d-flex flex-wrap justify-content-start'>
+        <AlbumTile/>
+        <AlbumTile/>
+        <AlbumTile/>
+        <AlbumTile/>
+        <AlbumTile/>
+        <AlbumTile/>
+       
+        
+        
+    </div>
+  )
+}
+
+export default AlbumList
