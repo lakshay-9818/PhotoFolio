8ea8f592824df72a36d514b1f4377d0134a0d60a
Lakshay Gupta <lakshaygupta3110@gmail.com>
2023-07-23 20:35:46 +0530

    minor issues resolved

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from "react";
-import { AlbumContext } from "../context/AlbumContext";
+import React from "react";
+
 import AlbumTile from "./AlbumTile";
 
 
